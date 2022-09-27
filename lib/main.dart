e4a5b6a15906b893ceefac7fd5deb494db5a48ec
Ketlen Rieper <ketlen.rieper@gmail.com>
2022-09-27 00:11:35 -0300

    Create Transaction class and file main()

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,53 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
 
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tênis de corrida',
+      value: 310.67,
+      date: DateTime.now(),
+    ),
+    Transaction(
+        id: 't2', title: 'Conta de luz', value: 120.99, date: DateTime.now())
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Personal Expenses'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
+              ),
+            ),
+            Card(
+              child: Text('Lista de transações'),
+            ),
+          ],
+        ));
+  }
+}
